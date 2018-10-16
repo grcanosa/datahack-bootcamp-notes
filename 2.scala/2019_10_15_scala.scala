8303b31e805108e467d39714cb60ceaa4f106803
grcanosa <grcanosa@gmail.com>
2018-10-16 18:27:19 +0200

    Added notes for scala session 1

--- 2.scala/2019_10_15_scala.scala
+++ 2.scala/2019_10_15_scala.scala
@@ -149,3 +149,69 @@ val stringToIntLambda: String => Int = {s=>s.toInt + 10}
 
 val stringToIntLambdaSugar: String => Int = {_.toInt + 10}
 
+// High order function
+
+
+//COMOPOSITION
+
+val add3:Int => Int = _ + 3
+val mult5:Int => Int = _ * 5
+
+val add3AndMul5: Int => Int = x => mult5(add3(x))
+val add3AndMul5_2:Int => Int = add3.andThen(mult5)
+val add3AndMul5_3:Int => Int = add3 andThen mult5
+
+//Partial functions
+val case1:PartialFunction[Int,String] = {
+  case 1 => "number1"
+}
+
+val case2:PartialFunction[Int,String] = {
+  case 2 => "number2"
+}
+
+val caseDef:PartialFunction[Int,String] = {
+  case _ => "numberDef"
+}
+
+val myCase = case1.orElse(case2).orElse(caseDef)
+val wrongCase = caseDef.orElse(case1).orElse(case2)
+
+//FIBONACCI TAIL RECURSIZE
+def fib(n:Int):Int = {
+  n match {
+    case 0 => 0
+    case 1 => 1
+    case n => fib(n-1)+fib(n-2)
+  }
+}
+
+//No se puede hacer tail recursive porque son dos llamadas
+
+fib(0)
+fib(1)
+fib(2)
+fib(3)
+fib(4)
+fib(5)
+
+//Coger una lista y darle la vuelta
+
+def reverse(l:List[Int]):List[Int] = l match {
+  case Nil => Nil
+  case head :: tail => reverse(tail) ::: List(head)
+}
+
+reverse(List(1,2,3,4,5))
+
+def reverseR(l:List[Int]):List[Int] = {
+  def revA(l:List[Int],acc:List[Int]):List[Int] = l match {
+    case Nil => acc
+    //Este caso no hace falta
+    //case head :: Nil => head :: acc
+    case head :: tail => revA(tail, head :: acc)
+  }
+  revA(l,Nil)
+}
+
+reverseR(List(1,2,3,4,5))
